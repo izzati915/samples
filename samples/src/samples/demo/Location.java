3c884a81c1c6e130cd01da55fb23bf0b79f9f7a1
Nur Izzati Binti Ishak <izzatichan915@gmail.com>
2022-03-11 23:57:10 +0800

    Initial Commit

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -2,6 +2,15 @@ package samples.demo;
 
 public class Location {
 private String name;
+private String address;
+
+public String getAddress() {
+	return address;
+}
+
+public void setAddress(String address) {
+	this.address = address;
+}
 
 public String getName() {
 	return name;
